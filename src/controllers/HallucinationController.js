7325beaeefe652249689766675e507862cc54dbf
strawberries1230 <45284542+strawberries1230@users.noreply.github.com>
2023-11-17 14:07:10 +0000

    fix save model date field missing & output model p_tuned false

--- src/controllers/HallucinationController.js
+++ src/controllers/HallucinationController.js
@@ -4,20 +4,26 @@ const mongoose = require('mongoose');
 
 const submitHallucination = async (req, res) => {
     try {
-        const { model_id, prompt, bad_response } = req.body;
+        // const { model_id, prompt, bad_response } = req.body;
+        const modelId = req.body.model_id;
+        const prompt = req.body.prompt;
+        const badResponse = req.body.bad_response;
         // Use the public key to verify the request (optional).
-        const mId = mongoose.Types.ObjectId(model_id);
-
+        const mId = mongoose.Types.ObjectId(modelId);
         const newSave = {
             model_id: mId,
             prompt: prompt,
-            bad_response: bad_response,
+            bad_response: badResponse,
             p_tuned: false,
+            date: new Date(),
         };
-
         // Save the hallucination data to your database (e.g., HALLUCINATION table).
         const hallucination = new Hallucination(newSave);
-        const saved = await hallucination.save();
+        // await hallucination.save();
+        hallucination
+            .save()
+            .then((doc) => console.log('Document saved:', doc))
+            .catch((err) => console.error('Error saving document:', err));
         // Return a success message.
         Utilities.apiResponse(
             res,
